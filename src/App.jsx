2d1af4acebe04c24b06ec8bf6c9af599282d51b5
Nik-web-frontend <nikhareyash38@gmail.com>
2025-07-13 17:39:26 +0530

    Minor changes
    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import Nav from './components/Nav'
+import React, { useState } from 'react'
+
 const App = () => {
+    const [stor, setStor] = useState({
+        name: '',
+        age: ''
+    });
+
+    function setVal(nm, val){
+        setStor({nm: {val}});
+
+    }
   return (
-    <>
-    <Nav/>
-      <h1>This is a header inside a fragments(empty tag)</h1>
-    </>
+    <div className='app'>
+        <form onSubmit={(e)=>{
+            e.preventDefault();
+            
+        }}>
+            <input type="text" placeholder='Enter Name' name='name' value={stor} onChange={(e)=>{setVal()}}/>
+            <input type='number' placeholder='Enter Age' name='age' value={stor} onChange={(e)=>{setStor([e.target.value])}}/>
+            <button>Submit</button>
+
+            <p>{stor}</p>
+        </form>
+    </div>
   )
 }
 
--- src/components/Nav.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Nav = () => {
-  return (
-  <h1>This is a header</h1>
-  )
-}
-
-export default Nav
