8d4acda7fb3ba522c6d65a9203f2b88b84e4797f
Nik-web-frontend <nikhareyash38@gmail.com>
2025-07-15 21:13:57 +0530

    Created login form
    Login form with validations and showing errors as well as form details.

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,101 @@ import React, { useState } from 'react'
 const App = () => {
     const [stor, setStor] = useState({
         name: '',
-        age: ''
+        contact: '',
+        email: '',
+        address: ''
     });
-
-    function setVal(nm, val){
-        setStor({nm: {val}});
+    const [res, setRes] = useState()
+    const [form, setForm] = useState(false)
+    const [showDetails, setShowDetails] = useState(false);
+    const [error, setError] = useState([]);
+    function setVal(nm, val) {
+        setStor(prev => ({ ...prev, [nm]: val }));
 
     }
-  return (
-    <div className='app'>
-        <form onSubmit={(e)=>{
-            e.preventDefault();
-            
-        }}>
-            <input type="text" placeholder='Enter Name' name='name' value={stor} onChange={(e)=>{setVal()}}/>
-            <input type='number' placeholder='Enter Age' name='age' value={stor} onChange={(e)=>{setStor([e.target.value])}}/>
-            <button>Submit</button>
+    function show(e) {
+        const temp = [];
+        e.preventDefault();
+        const name = /^[a-zA-Z]{3,}$/.test(stor.name)
+        const contact = /^[0-9]{10}$/.test(stor.contact)
+        if (!name) {
+            temp.push('Invalid Name!');
+        }
+        if (!contact) {
+            temp.push('Invalid Contact!');
+        }
 
-            <p>{stor}</p>
-        </form>
-    </div>
-  )
+
+        if (temp.length == 0) {
+            setRes(stor);
+            setStor({
+                name: '',
+                contact: '',
+                email: '',
+                address: ''
+            })
+            setForm(false)
+            setShowDetails(true)
+            setError([])
+        }
+        else {
+            (setError(temp))
+        }
+
+
+
+
+    }
+    return (
+        <div className='app'>
+
+
+            {!form && !showDetails && (
+                <button className='showForm' onClick={() => { setForm(true) }}>Show Form</button>
+            )}
+            {form && !showDetails && (
+                <form onSubmit={show}>
+                    <h2>Form</h2>
+
+                    <input type="text" placeholder='Enter Name (Atleast three letters)' name='name' value={stor.name} onChange={(e) => { setVal(e.target.name, e.target.value) }} required />
+
+                    <input type='number' placeholder='Enter Contact Number' name='contact' value={stor.contact} onChange={(e) => { setVal(e.target.name, e.target.value) }} required />
+                    <input type='email' placeholder='Enter Email (Only Valid Email Format)' name='email' value={stor.email} onChange={(e) => { setVal(e.target.name, e.target.value) }} required />
+
+                    <input type='text' placeholder='Enter Address' name='address' value={stor.address} onChange={(e) => { setVal(e.target.name, e.target.value) }} required />
+
+                    <button>Submit</button>
+                </form>
+            )}
+
+            {error && (
+                <div className="error-cont">
+
+                    {error.map((i, idx)=>{
+                        return <p key={idx} className='showErrors'>{i}</p>
+                    })}
+                </div>
+            )}
+
+            {!form && showDetails && (
+                <div className='viewDetails'>
+                    <div className="sub-cont">
+                        <h2>Form is submitted successfully!</h2>
+                        <p>Name: {res.name}</p>
+                        <p>Contact: {res.contact}</p>
+                        <p>E-mail: {res.email}</p>
+                        <p>Address: {res.address}</p>
+                        <button onClick={() => {
+                            setShowDetails(false)
+
+                        }}>Close</button>
+                    </div>
+
+                </div>
+            )}
+        </div>
+    )
 }
 
 export default App
+
